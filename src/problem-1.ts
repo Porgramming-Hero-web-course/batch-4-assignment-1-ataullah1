a7e28871483ba2fbb53009dc849457a69c4a8f09
Md Ataullah <ataullahm100@gmail.com>
2024-11-21 17:58:00 +0600

    Solved first problem

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,5 +1,8 @@
-function sumArray() {
-  return "Hello";
+function sumArray(arr: number[]): number {
+  const sum = arr.reduce((prev, curr) => prev + curr, 0);
+  return sum;
 }
 
-console.log(sumArray());
+const sum: number = sumArray([1, 2, 3, 4, 5]);
+
+console.log(sum);
